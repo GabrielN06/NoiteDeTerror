94be479ccdb28d13f4d03202b8f6c2740f92c083
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-06-23 12:32:34 -0300

    Programa Finalizado

--- Program.cs
+++ Program.cs
@@ -1,45 +1,44 @@
-﻿Console.WriteLine("--- Noite de Terror ---");
-
-int dia, mes;
-
-Console.Write("Qual o dia de nascimento da vítima (1 a 31): ");
-
-while (!int.TryParse(Console.ReadLine(), out dia) || dia <= 0 || dia > 31)
+﻿internal class Program
 {
-    Console.Write("Numero Inválido. Tente um número entre 1 e 31: ");
-}
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--- Noite de Terror ---");
 
-Console.Write("Qual o mês de nascimento da vítima (1 a 12): ");
+        int dia, mes;
 
-while (!int.TryParse(Console.ReadLine(), out mes) || mes <= 0 || mes > 12)
-{
-    Console.Write("Numero Inválido. Tente um número entre 1 e 12: ");
-}
+        Console.Write("Qual o dia de nascimento da vítima (1 a 31): ");
 
-string[] mesMonstros = {
-    "", "O Zumbi", "O Assassino", "O Psicopata", "O Alienígena",
-    "O Carniceiro","O Matador", "O Vampiro", "O Maluco",
-    "O Vingador", "O Monstro","O Bruxo", "O Demônio"
-};
+        while (!int.TryParse(Console.ReadLine(), out dia) || dia <= 0 || dia > 31)
+        {
+            Console.Write("Número Inválido. Tente um número entre 1 e 31: ");
+        }
 
-string[] diaSobrenome =
-{
-    "", "Endiabrado", "Radioativo", "Endemoniado", "Vermelho", "Possuído",
-    "Macabro", "Sombrio", "Sem Cabeça", "Desconhecido", "Inconsciente",
-    "Oculto", "Esquecido", "Lúgubre", "Invocado", "Caído", "Costurado",
-    "Amaldiçoado", "Anormal", "Perturbado", "Sanguinário", "Tenebroso",
-    "Imortal", "Ressuscitado", "do Pântano", "da Encruzilhada", "do Porão",
-    "do Velho Poço", "sem Perdão", "do Cemitério", "da Meia-Noite", "da Lua Cheia"
-};
+        Console.Write("Qual o mês de nascimento da vítima (1 a 12): ");
 
-if (mes >= 1 && mes <= 12 && dia >= 1 && dia <= 31)
-{
-    string nomeMonstro = mesMonstros[mes];
-    string sobrenome = diaSobrenome[dia];
-    Console.WriteLine($"\nFuja! Lá vem {nomeMonstro} {sobrenome}!");
-}
+        while (!int.TryParse(Console.ReadLine(), out mes) || mes <= 0 || mes > 12)
+        {
+            Console.Write("Número Inválido. Tente um número entre 1 e 12: ");
+        }
 
-else
-{
-    Console.WriteLine("\nData Inváldia, por favor, insira um dia (1-31) e mês (1-12) válidos.");
+        string[] mesMonstros = {
+            "", "O Zumbi", "O Assassino", "O Psicopata", "O Alienígena",
+            "O Carniceiro","O Matador", "O Vampiro", "O Maluco",
+            "O Vingador", "O Monstro","O Bruxo", "O Demônio"
+        };
+
+        string[] diaSobrenome =
+        {
+             "", "Endiabrado", "Radioativo", "Endemoniado", "Vermelho", "Possuído",
+             "Macabro", "Sombrio", "Sem Cabeça", "Desconhecido", "Inconsciente",
+             "Oculto", "Esquecido", "Lúgubre", "Invocado", "Caído", "Costurado",
+             "Amaldiçoado", "Anormal", "Perturbado", "Sanguinário", "Tenebroso",
+             "Imortal", "Ressuscitado", "do Pântano", "da Encruzilhada", "do Porão",
+             "do Velho Poço", "sem Perdão", "do Cemitério", "da Meia-Noite", "da Lua Cheia"
+        };
+
+
+        string nomeMonstro = mesMonstros[mes];
+        string sobrenome = diaSobrenome[dia];
+        Console.WriteLine($"\nFuja! Lá vem {nomeMonstro} {sobrenome}!");
+    }
 }
